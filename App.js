234087a3429ee9ebe24016870d34b1177680e09c
Henish11 <henish09@gmail.com>
2023-05-22 02:13:51 +0530

    component added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Component from "./src/component";
 
 const App = () => {
     return (
         <>
         <h1>hello</h1>
+        <Component/>
         </>
     )
 }
---
+++ src/component.js
@@ -0,0 +1,11 @@
+
+
+const Component = ()=>{
+    return(
+        <>
+          <h1>Hellowss</h1>
+        </>
+    )
+}
+
+export default Component
